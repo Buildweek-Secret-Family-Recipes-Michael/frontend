be036bd89bb0df7851a17be4207ce2240c4f6419
Alex Andrew <pytormal@live.com>
2020-09-17 16:00:57 -0700

    added routes to home and userForm for login , created home page

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+// import UserForm from './components/Form';
 
 import "./App.css";
 
 function App() {
-  return <div>//Handle all routes and links in here.</div>;
+ return (
+    <div ClassName="App">
+<h1>The Secret Family Recipes</h1>
+     <p> add your family recipe cards here!</p>
+
+       <nav>
+      <Link to="/">Login</Link>
+      <Link to="/Home">Home</Link>
+</nav>
+  
+
+    <Switch>
+         <Route path="/Home">
+          <Home />
+        </Route>
+        {/* <Route exact path="/">
+          <UserForm/>
+        </Route> */}
+      </Switch> 
+    
+
+       </div >
+  );
 }
 
 export default App;
