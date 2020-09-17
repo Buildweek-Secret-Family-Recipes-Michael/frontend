b1a1a7ce2e4ce84e67f37e9d616539fbb2421b4b
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-09-16 19:55:40 -0500

    added placeholder js files

---
+++ secret-family-recipes/src/components/EditDelete.js
@@ -0,0 +1,1 @@
+// This is where editing and deleting functionality will reside. 
---
+++ secret-family-recipes/src/components/UserForm.js
@@ -0,0 +1,3 @@
+// This is where the user onboarding form will live. UserForm will authenticate user and push them to main page. 
+
+// Once pushed to the main page, the user can see all recipe cards in database, then they can click on navbar add recipe link to take them to add recipe form page. 
