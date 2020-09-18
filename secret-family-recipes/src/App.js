a27256ef4f069ad89f7a4c5c72862b3306320b57
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-09-18 14:33:43 -0500

    Created context data flow model from App to Recipe cards

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
-
+import React, {useState, createContext} from "react";
+import axios from 'axios'
 import "./App.css";
 
+export const RecipeContext = createContext()
+
 function App() {
-  return <div>//Handle all routes and links in here.</div>;
+  const [recipes, setRecipes] = useState([]) // Will use whatever data we pull in from the get request here. 
+
+  const getRecipes = () => {
+    axios
+      .get("")
+      .then((res) => setRecipes(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+  return (
+    <div>
+      <RecipeContext.Provider value={recipes}>
+        //Handle all routes and links in here.
+      </RecipeContext.Provider>
+      
+      </div>
+  )
 }
 
 export default App;
