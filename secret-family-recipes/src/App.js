e2a6634163d524ac5e88b3d1e76916348f641f10
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-09-21 21:38:03 -0400

    PrivateRoute merge fix for App.js file

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -41,15 +41,15 @@ function App() {
           <h1>The Secret Family Recipes</h1>
 
           <nav>
-            <Link to="/Home">Home</Link>
+            <Link to="/protected">Home</Link>
             <Link to="/">Login</Link>
             <Link to="/Register">Register Here</Link>
           </nav>
 
           <Switch>
-            <Route path="/Home">
+            <PrivateRoute exact path="/protected" component={Home}>
               <Home />
-            </Route>
+            </PrivateRoute>
             <Route path="/AddRecipe">
               <AddRecipe />
             </Route>
